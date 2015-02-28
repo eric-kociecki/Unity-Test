49df8feb8f43da24904bd5fdfdd1512c74e0f418
Tedris <vanloone@gmail.com>
2015-02-28 14:49:20 -0500

    Slightly tweaked AI
    Will follow over blocks, kind of funny looking.
    
    2d sprites are kind of hard to animate like this though.

--- Assets/Standard Assets/Scripts/EnemyAI.cs
+++ Assets/Standard Assets/Scripts/EnemyAI.cs
@@ -23,10 +23,14 @@ public class EnemyAI : MonoBehaviour {
 			// Create a vector to the player position
 			Vector3 deltaPosition = this.transform.position - player.transform.position;
 
+			//figure out how to tell if there's terrain collision in between AI and player of y > 2
+
 			if (deltaPosition.sqrMagnitude > 1.25f)
 			{
 				Debug.Log ("Player is within perception sphere, moving to intercept");
-				this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, .05f);
+
+				//will need to change this when implementing A*, should follow given path instead
+				this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, .075f);
 			}
 		}
 	}
